8e03c495005019e1f0025a5d6e422b6cd86a7915
Chris King <chris.king@razorfish.com>
2014-03-25 14:39:17 +0000

    20140325T1439 updated aem cookbook to remove apt recipe dependency.

--- aem/metadata.rb
+++ aem/metadata.rb
@@ -9,6 +9,6 @@ version          '0.1.4'
 recipe 'master', 'Installs AEM and dependencies'
 
 # If we reference these elsewhere, e.g. in recipes, you need to list them as dependencies here
-depends 'apt'
+#depends 'apt'
 depends 'apache2'
 depends 'opsworks_java'
